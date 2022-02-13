98a25d5e1768ce387d9e2349ee4d1a0ba6e2f48b
Levy Nunes <levy.nunes@squidit.com.br>
2022-02-13 01:21:17 -0300

    create register component

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,10 +1,34 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
   selector: 'mc-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+
+  form: FormGroup
+
+  constructor(
+    private fb: FormBuilder
+  ) {}
+
+  ngOnInit(): void {
+      this.initializeForm()
+  }
+
+  initializeForm(): void {
+    console.log('NgRx')
+    this.form = this.fb.group({
+      username: ['', Validators.required],
+      email: ['', Validators.email],
+      password: ['', Validators.required]
+    })
+  }
+
+  onSubmit(){
+    console.log('submit', this.form.value, this.form.valid)
+  }
 
 }
